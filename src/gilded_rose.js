59d4207b3fc5cc9e7f75671ec8bfc5e3f7256394
mish3n <mish3n@gmail.com>
2021-03-04 17:38:16 +0200

    refactoring

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -28,52 +28,52 @@ class Shop {
 
     updateRegularItemQuality(i) {
         if (this.items[i].quality > 0) {
-            this.items[i].quality = this.items[i].quality - 1;
+            --this.items[i].quality;
         }
 
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        --this.items[i].sellIn;
 
         if (this.items[i].sellIn < 0) {
             if (this.items[i].quality > 0) {
-                this.items[i].quality = this.items[i].quality - 1;
+                --this.items[i].quality;
             }
         }
     }
 
     updateBackstagePassQuality(i) {
         if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            ++this.items[i].quality;
             if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].sellIn < 11) {
                     if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
+                        ++this.items[i].quality;
                     }
                 }
                 if (this.items[i].sellIn < 6) {
                     if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
+                        ++this.items[i].quality;
                     }
                 }
             }
         }
 
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        --this.items[i].sellIn;
 
         if (this.items[i].sellIn < 0) {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality = 0;
         }
     }
 
     updateAgedBrie(i) {
         if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+            ++this.items[i].quality;
         }
 
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        --this.items[i].sellIn;
 
         if (this.items[i].sellIn < 0) {
             if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+                ++this.items[i].quality;
             }
         }
     }
