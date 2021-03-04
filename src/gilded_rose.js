318ff8f4e2b96463b14adfe969427e764c59be5f
mish3n <mish3n@gmail.com>
2021-03-04 17:31:36 +0200

    removed unreachable backstage passes checks

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -61,37 +61,15 @@ class Shop {
                     }
                 }
             } else {
-                if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (this.items[i].quality > 0) {
-                        this.items[i].quality = this.items[i].quality - 1;
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1;
-                        if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                            if (this.items[i].sellIn < 11) {
-                                if (this.items[i].quality < 50) {
-                                    this.items[i].quality = this.items[i].quality + 1;
-                                }
-                            }
-                            if (this.items[i].sellIn < 6) {
-                                if (this.items[i].quality < 50) {
-                                    this.items[i].quality = this.items[i].quality + 1;
-                                }
-                            }
-                        }
-                    }
+                if (this.items[i].quality > 0) {
+                    this.items[i].quality = this.items[i].quality - 1;
                 }
 
                 this.items[i].sellIn = this.items[i].sellIn - 1;
 
                 if (this.items[i].sellIn < 0) {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            this.items[i].quality = this.items[i].quality - 1;
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality;
+                    if (this.items[i].quality > 0) {
+                        this.items[i].quality = this.items[i].quality - 1;
                     }
                 }
             }
